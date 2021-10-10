271aee829682f66dbd5c438229b9446ba1f713a3
Joan Killeen <joan.killeen@bluewin.ch>
2021-10-10 17:43:42 +0200

    toString for item

--- src/main/java/com/gildedrose/Item.java
+++ src/main/java/com/gildedrose/Item.java
@@ -1,21 +1,23 @@
 package com.gildedrose;
 
+import java.text.MessageFormat;
+
 public class Item {
 
-    public String name;
+	public String name;
 
-    public int sellIn;
+	public int sellIn;
 
-    public int quality;
+	public int quality;
 
-    public Item(String name, int sellIn, int quality) {
-        this.name = name;
-        this.sellIn = sellIn;
-        this.quality = quality;
-    }
+	public Item(String name, int sellIn, int quality) {
+		this.name = name;
+		this.sellIn = sellIn;
+		this.quality = quality;
+	}
 
-   @Override
-   public String toString() {
-        return this.name + ", " + this.sellIn + ", " + this.quality;
-    }
+	@Override
+	public String toString() {
+		return MessageFormat.format("name: {0}, sellIn: {1}, quality: {2}", this.name, this.sellIn, this.quality);
+	}
 }
