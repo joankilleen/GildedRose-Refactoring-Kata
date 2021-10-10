36bf01ce26acf959137864928c95e635e181a49d
Joan Killeen <joan.killeen@bluewin.ch>
2021-10-10 17:09:57 +0200

    adding unit tests

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -7,7 +7,7 @@ import java.util.stream.Collectors;
 import org.apache.commons.lang3.Validate;
 
 public class GildedRose {
-	Item[] items = new Item[100];
+	Item[] items = new Item[1];
 
 	public GildedRose(Item[] items) {
 		Validate.notNull(items);
@@ -17,6 +17,7 @@ public class GildedRose {
 	public GildedRose(List<Item> listItems) {
 		Validate.notNull(listItems);
 		int i = 0;
+		items = new Item[listItems.size()];
 		for (Item listItem : listItems) {
 			items[i++] = listItem;
 
--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -2,6 +2,7 @@ package com.gildedrose;
 
 import static org.junit.Assert.assertTrue;
 
+import java.text.MessageFormat;
 import java.util.List;
 
 import org.junit.Before;
@@ -17,17 +18,32 @@ public class GildedRoseTest {
 	}
 
 	@Test
-	public void foo() {
-		System.out.println("Test can be called");
+	public void testQualitySellInDecrement() {
+		// arrange
+		Item item3 = app.getItems().get(2);
+		int qualityBeforeUpdate = item3.quality;
+		int sellInBeforeUpdate = item3.sellIn;
+		// act
+		app.updateQuality();
+		// assert
+		assertTrue(item3.quality == qualityBeforeUpdate - 1);
+		assertTrue(item3.sellIn == sellInBeforeUpdate - 1);
+
 	}
 
 	@Test
-	public void testSulfuras() {
-
-		assert app != null;
-		assert app.getItems() != null;
+	public void testSulfurasQualityDoesntChange() {
 		List<Item> sulfuras = app.searchItems("Sulfuras");
 		assertTrue(sulfuras.size() == 2);
+		int qualityBeforeUpdate = sulfuras.get(0).quality;
+		System.out.println(MessageFormat.format("Quality before update: {0}", qualityBeforeUpdate));
+
+		// Update quality for 5 days
+		for (int i = 5; i > 0; i--) {
+			app.updateQuality();
+		}
+		int qualityAfterUpdate = app.searchItems("Sulfuras").get(0).quality;
+		assertTrue(qualityAfterUpdate == qualityBeforeUpdate);
 
 	}
 }
